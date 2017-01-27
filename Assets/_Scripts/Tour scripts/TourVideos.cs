5a85d2d6e5f0264af1634593c589dbe0cb988f2e
Fire3galaxy <daniel_handojo@hotmail.com>
2017-01-26 16:16:55 -0800

    Reenabled video playback

--- Assets/_Scripts/Tour scripts/TourVideos.cs
+++ Assets/_Scripts/Tour scripts/TourVideos.cs
@@ -9,7 +9,9 @@ public class TourVideos : Photon.MonoBehaviour {
     void Start() {
         mediaPlayer = GetComponent<MediaPlayerCtrl>();
         mediaPlayer.OnReady += OnLoadedVideo;
+        mediaPlayer.OnVideoError += OnVideoError;
         mediaPlayer.Load(FirstVideo);
+        //mediaPlayer.Load("C:///Users/Daniel/AppData/LocalLow/Sky/MyVR/video.mp4");
     }
 
     // Make a text file of video file names so I do not 
@@ -20,11 +22,17 @@ public class TourVideos : Photon.MonoBehaviour {
     }
 
     public void PlayVideo() {
-        //mediaPlayer.Play();
+        mediaPlayer.Play();
     }
 
     private void OnLoadedVideo() {
-        //if (playOnLoad)
-        //    mediaPlayer.Play();
+        if (playOnLoad)
+            mediaPlayer.Play();
+        Debug.Log("Ready");
+    }
+
+    private void OnVideoError(MediaPlayerCtrl.MEDIAPLAYER_ERROR errorCode, 
+            MediaPlayerCtrl.MEDIAPLAYER_ERROR errorCodeExtra) {
+        Debug.Log("Error with loading video: " + errorCode + ", " + errorCodeExtra);
     }
 }
